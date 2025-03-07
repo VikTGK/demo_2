10e81f8098b0b2e92c51c76c65d6d64ce2532946
viktor <viktortaganashkin@gmail.com>
2025-03-07 11:23:01 +0100

    comit3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String name;
-        int number
+        int number;
         System.out.println("Hello, World!");
 
     }
